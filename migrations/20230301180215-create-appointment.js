9fe32261475de6db1c7d871c57ee00e7af43b618
Ignacio Furió José <bichoifj@gmail.com>
2023-03-01 19:03:05 +0100

    feat: created Appointment model and migration

---
+++ migrations/20230301180215-create-appointment.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Appointments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date_time: {
+        type: Sequelize.DATE
+      },
+      patient_id: {
+        type: Sequelize.INTEGER
+      },
+      doctor_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Appointments');
+  }
+};
