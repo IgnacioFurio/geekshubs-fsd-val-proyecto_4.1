feae84bf9df9d8580b6fa0ab095dfd568cebbf04
Ignacio Furió José <bichoifj@gmail.com>
2023-03-01 17:39:48 +0100

    feat: created model and migrations for Doctor

---
+++ migrations/20230301161925-create-doctor.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Doctors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      collegiate_member: {
+        type: Sequelize.INTEGER
+      },
+      contract_at: {
+        type: Sequelize.DATEONLY
+      },
+      contract_until: {
+        type: Sequelize.DATEONLY
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key:"id",
+          allowNull: false
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Doctors');
+  }
+};
---
+++ models/doctor.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Doctor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Doctor.init({
+    collegiate_member: DataTypes.INTEGER,
+    contract_at: DataTypes.DATEONLY,
+    contract_until: DataTypes.DATEONLY,
+    user_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Doctor',
+  });
+  return Doctor;
+};
