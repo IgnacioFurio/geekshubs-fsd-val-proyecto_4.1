50327e48f26845a50b5c34e41d59c415c494cb62
Ignacio Furió José <bichoifj@gmail.com>
2023-03-03 14:17:27 +0100

    feat: userLogin controller prepared

--- views/authRoutes.js
+++ views/authRoutes.js
@@ -3,7 +3,7 @@ const authController = require('../controllers/authController');
 const router = require('express').Router();
 
 router.post('/register', authController.createUserProfile);
-router.get('/', authController.getUserProfile);
+router.post('/login', authController.userLogin);
 router.put('/', authController.updateUserProfile);
 
 module.exports = router;
