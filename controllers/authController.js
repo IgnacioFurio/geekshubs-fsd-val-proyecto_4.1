f7303f872bcbac0eb6377ca5fce4604f5e6dd8d6
corina <ciorpaccorina@gmail.com>
2023-03-03 09:08:47 +0100

    feat: createuserProfile function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,29 @@
 const authController = {};
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-authController.createUserProfile = (req,res) => {return res.send('Crear User')};
+authController.createUserProfile = async (req, res) => {
+    try {
+    // Recuperar información de la petición
+    const { name, email, password } = req.body;
+    // Cifrar la contraseña
+    const encryptedPassword = bcrypt.hashSync(password, 10);
+    // Crear un nuevo usuario en la base de datos
+    const newUser = await User.create({
+        name,
+        email,
+        password: encryptedPassword,
+      role_id: 1 // Establecer el rol del usuario
+    });
+    // Devolver la información del nuevo usuario
+    return res.json(newUser);
+    } catch (error) {
+    // Enviar un mensaje de error si hay algún problema
+    return res.status(500).send(error.message);
+    } 
+};
+
 authController.getUserProfile = (req,res) => {return res.send('Ver perfil de User')};
 authController.updateUserProfile = (req,res) => {return res.send('Cambiar información de User')};
 
