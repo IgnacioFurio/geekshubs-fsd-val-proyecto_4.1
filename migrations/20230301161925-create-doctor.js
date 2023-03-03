aad27dc339e389ba5c3b4d69cdbd452db2e57bb2
Ignacio Furió José <bichoifj@gmail.com>
2023-03-03 19:55:32 +0100

    feat: Doctor has now seeder

--- migrations/20230301161925-create-doctor.js
+++ migrations/20230301161925-create-doctor.js
@@ -10,7 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       collegiate_member: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       contract_at: {
         type: Sequelize.DATEONLY
