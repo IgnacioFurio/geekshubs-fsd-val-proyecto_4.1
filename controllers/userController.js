ce50fc0f532b4ea7d97cf683256682540b8e3bef
Ignacio Furió José <bichoifj@gmail.com>
2023-03-04 18:56:49 +0100

    fix: getUserProfile shows profile of the logged user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,20 +3,39 @@ const userController = {};
 
 userController.getUserProfile =  async (req, res) => {
     try {
-      const userId = req.userId; 
-      const user = await User.findOne({ id: userId } );
+      const userId = req.userId;
+
+      const user = await User.findByPk(
+        userId,
+        {
+          attributes: 
+          {
+            exclude: ["password"]
+          },
+        }
+      );
   
       if (!user) {
         return res.status(404).json({ message: 'User not found' });
       }
   
-      return res.json(user);
+      return res.json(
+        {
+          succes: true,
+          message: 'User information',
+          data: user
+        }
+      );
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ message: 'Internal server error' });
+      return res.status(500).json(
+        { 
+          message: 'Internal server error' 
+        }
+      );
     }
   };
-  
+
 userController.updateUserProfile = (req,res) => {return res.send('Cambiar datos de Usuario')};
 
 module.exports = userController;
