d76e0a16db08befb4962c5b292712740e3c29781
Ignacio Furió José <bichoifj@gmail.com>
2023-03-04 18:20:31 +0100

    fix: verifyToken

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,14 +2,15 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const authorization = req.headers.authorization;
 
-    if (!token) {
+    if (!authorization) {
       return res.status(401).json({ message: 'No token provided' });
     }
 
+    const [ bearer, token ]  = authorization.split(" ")
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    console.log(decoded);
     // Añadir la información del usuario decodificada al objeto de solicitud para su posterior uso en otros controladores
     req.userId = decoded.userId;
     req.userName = decoded.userName;
