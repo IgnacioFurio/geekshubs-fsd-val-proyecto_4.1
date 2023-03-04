373b566ba139e624f6b75494e63938c697cc517d
Ignacio Furió José <bichoifj@gmail.com>
2023-03-04 11:51:02 +0100

    feat: coming back to a previous point

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,42 +1,6 @@
-const { Patient } = require('../models');
-
 const patientController = {};
 
-patientController.createPatient = (req,res) => {
-    try {
-        const {name, surname, DNI, phone_number, post_code, birth, allergy, surgery, user_id} = req.body;
-
-        const newPatient = {
-            name: name,
-            surname: surname,
-            DNI: DNI,
-            phone_number: phone_number,
-            post_code: post_code,
-            birth: birth,
-            allergy: allergy,
-            surgery: surgery,
-            user_id: user_id
-        };
-
-        const patient = Patient.create(newPatient)
-
-        return res.json(
-            {
-                succes: true,
-                message: 'New Patient file created',
-                data: patient
-            }
-        );
-    } catch (error) {
-        return res.status(500).json(
-            {
-                succes: true,
-                message: "Something went wrong",
-                error: error.message
-            })
-        }
-};
-
+patientController.createPatient = (req,res) => {return res.send('create patient information')};
 patientController.getPatientInfo = (req,res) => {return res.send('get patient information')};
 patientController.getPatientAppointment = (req,res) => {return res.send('Póximas citas')};
 
