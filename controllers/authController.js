0bbb4e22b900eb5f8895bdf03f7e473b7a0da736
Ignacio Furió José <bichoifj@gmail.com>
2023-03-05 09:33:45 +0100

    fix: userLogin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const authController = {};
-const { User } = require('../models/appointment');
+const { User } = require('../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
