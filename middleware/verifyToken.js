b89cd366f3a64d247250ef333a2ce81be55fe279
Ignacio Furió José <bichoifj@gmail.com>
2023-03-05 23:28:08 +0100

    chore: add error messages in verifyToken

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,25 +1,38 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
+  
   try {
+
     const authorization = req.headers.authorization;
 
     if (!authorization) {
-      return res.status(401).json({ message: 'No token provided' });
-    }
+      return res.status(505).json(
+        { 
+          succes: false,
+          message: 'You shall not pass!!.' 
+        }
+      );
+    };
 
     const [ bearer, token ]  = authorization.split(" ")
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    // Añadir la información del usuario decodificada al objeto de solicitud para su posterior uso en otros controladores
+
     req.userId = decoded.userId;
     req.userName = decoded.userName;
     req.roleId = decoded.roleId;
 
     next();
   } catch (error) {
-    return res.status(500).send(error.message)
-  } 
+    return res.status(500).json(
+      {
+        succes: false,
+        message: "Somethin went wrong.",
+        error: error.message
+      }
+    );
+  };
 };
 
 module.exports = verifyToken;
