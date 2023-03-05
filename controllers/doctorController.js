1bc760dfc2ad746b2710a7d18d30c1c45a7e08b7
Ignacio Furió José <bichoifj@gmail.com>
2023-03-05 10:46:58 +0100

    feat: findAll Doctor in getDoctorAppointment

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,5 +1,28 @@
+const { Doctor } = require('../models')
+
 const doctorController = {};
 
-doctorController.getDoctorAppointment = (req,res) => {return res.send('Próximas citas a atender')}; 
+doctorController.getDoctorAppointment = async (req,res) => {
+    try {
+        const doctor = req.userId;
+        console.log(doctor);
+        const newConsult = await Doctor.findAll()
+
+        return res.json(
+        {
+            succes: true,
+            message: 'Doctors Appointment',
+            data: newConsult
+        }
+    );
+    } catch (error) {
+        return res.json(
+            {
+                succes: false,
+                message: error.message
+            }
+        )
+    }
+}; 
 
 module.exports = doctorController;
--- views/doctorRoutes.js
+++ views/doctorRoutes.js
@@ -1,7 +1,8 @@
 const doctorController = require('../controllers/doctorController');
+const verifyToken = require('../middleware/verifyToken')
 
 const router = require('express').Router();
 
-router.get('/appointment/', doctorController.getDoctorAppointment);
+router.get('/appointment/', verifyToken, doctorController.getDoctorAppointment);
 
 module.exports = router;
