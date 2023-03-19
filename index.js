1e666a99a9f600aa52d4fd777be1a1c58c7243ad
Ignacio Furió José <bichoifj@gmail.com>
2023-03-19 11:37:34 +0100

    feat: add cors to the API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const db = require('./db.js');
 require('dotenv').config()
@@ -7,8 +8,18 @@ const router = require('./router')
 
 const PORT = process.env.PORT;
 
+let corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+    optionsSuccessStatus: 204
+};
+
 app.use(express.json());
-app.use(router)
+app.use(cors(corsOptions));
+app.use(router);
 
 db.then(() => {
     app.listen(PORT, () => console.log("Server on port " + PORT));
