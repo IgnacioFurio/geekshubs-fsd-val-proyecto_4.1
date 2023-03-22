433c02a6e7163092912bf8fdd8bf41cd412c544a
corina <ciorpaccorina@gmail.com>
2023-03-22 11:03:46 +0100

    chore railway

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ require('dotenv').config()
 const router = require('./router')
 
 const PORT = process.env.PORT;
+const cors = require ("cors")
+let corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions))
 
 app.use(express.json());
 app.use(router)
